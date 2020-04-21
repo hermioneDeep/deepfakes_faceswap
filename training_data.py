a66d809493baf206a85fb13e19b9f36a8be08402
fff <dimajkovlev865@gmail.com>
2020-04-22 01:24:28 +0300

    Revert "new loader"
    This reverts commit 9c126450fd1aa2b7ea4a51359cfbb83027def608.

--- training_data.py
+++ training_data.py
@@ -11,15 +11,28 @@ random_transform_args = {
     }
 
 
-def get_train_images()
+class ArtificialDataset(tf.data.Dataset):
+    def _generator(num_samples):
+    image_provider = get_images()
+    for image in image_provider:
+        image = random_transform( image, **random_transform_args )
+        warped_img, target_img = random_warp( image )
+        yield warped_img, target_img    
+    def __new__(cls, num_samples=3):
+        return tf.data.Dataset.from_generator(
+            cls._generator,
+            output_types=tf.dtypes.int64,
+            output_shapes=(1,),
+            args=(num_samples,)
+        )
+
+
+def get_train_images():
     image_provider = get_images()
     for image in image_provider:
         image = random_transform( image, **random_transform_args )
         warped_img, target_img = random_warp( image )
         yield warped_img, target_img
-
-ds_counter = tf.data.Dataset.from_generator(count, args=[25], output_types=tf.int32, output_shapes = (), )
-
 
 def get_training_data( images, batch_size ):
     indices = numpy.random.randint( len(images), size=batch_size )
