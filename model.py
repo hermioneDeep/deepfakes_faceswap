16b70194db21f00ec144c8d5f098e39ff5ce814c
fff <dimajkovlev865@gmail.com>
2020-04-23 21:13:41 +0300

    Villain model, tpu disabled

--- model.py
+++ model.py
@@ -2,11 +2,16 @@ import tensorflow as tf
 import os
 
 from tensorflow.python.keras.models import Model
-from tensorflow.python.keras.layers import Input, Dense, Flatten, Reshape
+from tensorflow.python.keras.layers import Input, Dense, Flatten, Reshape, 
 from tensorflow.python.keras.layers.advanced_activations import LeakyReLU
 from tensorflow.python.keras.layers.convolutional import Conv2D
 from tensorflow.keras.optimizers import Adam
 from pixel_shuffler import PixelShuffler
+from model_tools import NNBlocks
+from tf.keras.initializers import RandomNormal
+
+blocks = NNBlocks()
+
 #from tensorflow.python.framework.ops import disable_eager_execution
 
 #disable_eager_execution()
@@ -16,64 +21,148 @@ optimizer = Adam( lr=5e-5, beta_1=0.5, beta_2=0.999 )
 IMAGE_SHAPE = (64,64,3)
 ENCODER_DIM = 1024
 
-def conv( filters ):
-    def block(x):
-        x = Conv2D( filters, kernel_size=5, strides=2, padding='same' )(x)
-        x = LeakyReLU(0.1)(x)
-        return x
-    return block
+#def conv( filters ):
+#    def block(x):
+#        x = Conv2D( filters, kernel_size=5, strides=2, padding='same' )(x)
+#        x = LeakyReLU(0.1)(x)
+#        return x
+#    return block
 
-def upscale( filters ):
-    def block(x):
-        x = Conv2D( filters*4, kernel_size=3, padding='same' )(x)
-        x = LeakyReLU(0.1)(x)
-        x = PixelShuffler()(x)
-        return x
-    return block
+#def upscale( filters ):
+#    def block(x):
+#        x = Conv2D( filters*4, kernel_size=3, padding='same' )(x)
+#        x = LeakyReLU(0.1)(x)
+#        x = PixelShuffler()(x)
+#        return x
+#    return block
+#
+#def Encoder():
+#    input_ = Input( shape=IMAGE_SHAPE )
+#    x = input_
+#    x = conv( 128)(x)
+#    x = conv( 256)(x)
+#    x = conv( 512)(x)
+#    x = conv(1024)(x)
+#    x = Dense( ENCODER_DIM )( Flatten()(x) )
+#    x = Dense(4*4*1024)(x)
+#    x = Reshape((4,4,1024))(x)
+#    x = upscale(512)(x)
+#    return Model( input_, x )
+#
+#def Decoder():
+#    input_ = Input( shape=(8,8,512) )
+#    x = input_
+#    x = upscale(256)(x)
+#    x = upscale(128)(x)
+#    x = upscale( 64)(x)
+#    x = Conv2D( 3, kernel_size=5, padding='same', activation='sigmoid' )(x)
+#    return Model( input_, x )
 
-def Encoder():
-    input_ = Input( shape=IMAGE_SHAPE )
-    x = input_
-    x = conv( 128)(x)
-    x = conv( 256)(x)
-    x = conv( 512)(x)
-    x = conv(1024)(x)
-    x = Dense( ENCODER_DIM )( Flatten()(x) )
-    x = Dense(4*4*1024)(x)
-    x = Reshape((4,4,1024))(x)
-    x = upscale(512)(x)
-    return Model( input_, x )
 
-def Decoder():
-    input_ = Input( shape=(8,8,512) )
-    x = input_
-    x = upscale(256)(x)
-    x = upscale(128)(x)
-    x = upscale( 64)(x)
-    x = Conv2D( 3, kernel_size=5, padding='same', activation='sigmoid' )(x)
-    return Model( input_, x )
+#kwargs["input_shape"] = (128, 128, 3)
+#kwargs["encoder_dim"] = 512 if config["lowmem"] else 1024
+kernel_initializer = RandomNormal(0, 0.02)
+input_shape = (128, 128, 3)
+encoder_dim = 1024
+def encoder():
+    """ Encoder Network """
+    global kernel_initializer
+    global input_shape
+    kwargs = dict(kernel_initializer=kernel_initializer)
+    input_ = Input(shape=input_shape)
+    in_conv_filters = input_shape[0]
+    dense_shape = input_shape[0] // 16
 
-resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu='grpc://' + os.environ['COLAB_TPU_ADDR'])
-tf.config.experimental_connect_to_cluster(resolver)
-# This is the TPU initialization code that has to be at the beginning.
-tf.tpu.experimental.initialize_tpu_system(resolver)
-strategy = tf.distribute.experimental.TPUStrategy(resolver)
-try:
-    with strategy.scope():
-        encoder = Encoder()
-        decoder_A = Decoder()
-        decoder_B = Decoder()
+    var_x = blocks.conv(input_, in_conv_filters, res_block_follows=True, **kwargs)
+    tmp_x = var_x
+    res_cycles = 16
+    for _ in range(res_cycles):
+        nn_x = blocks.res_block(var_x, in_conv_filters, **kwargs)
+        var_x = nn_x
+    # consider adding scale before this layer to scale the residual chain
+    var_x = add([var_x, tmp_x])
+    var_x = blocks.conv(var_x, 128, **kwargs)
+    var_x = PixelShuffler()(var_x)
+    var_x = blocks.conv(var_x, 128, **kwargs)
+    var_x = PixelShuffler()(var_x)
+    var_x = blocks.conv(var_x, 128, **kwargs)
+    var_x = blocks.conv_sep(var_x, 256, **kwargs)
+    var_x = blocks.conv(var_x, 512, **kwargs)
+    var_x = blocks.conv_sep(var_x, 1024, **kwargs)
+    var_x = Dense(encoder_dim, **kwargs)(Flatten()(var_x))
+    var_x = Dense(dense_shape * dense_shape * 1024, **kwargs)(var_x)
+    var_x = Reshape((dense_shape, dense_shape, 1024))(var_x)
+    var_x = blocks.upscale(var_x, 512, **kwargs)
+    return Model(input_, var_x)
 
-        x = Input( shape=IMAGE_SHAPE )
-        y = Input( shape=IMAGE_SHAPE )
-        #autoencoder_A = Model( x, decoder_A( encoder(x) ) )
-        #autoencoder_B = Model( x, decoder_B( encoder(x) ) )
-        dual_model = Model(inputs=[x, y],
-                            outputs=[decoder_A(encoder(x)), decoder_B(encoder(y))])
-        dual_model.compile(optimizer=optimizer, loss='mean_absolute_error')
+def decoder_A():
+    """ Decoder Network """
+    global kernel_initializer
+    global input_shape
+    kwargs = dict(kernel_initializer=kernel_initializer)
+    decoder_shape = input_shape[0] // 8
+    input_ = Input(shape=(decoder_shape, decoder_shape, 512))
+
+    var_x = input_
+    var_x = blocks.upscale(var_x, 512, res_block_follows=True, **kwargs)
+    var_x = blocks.res_block(var_x, 512, **kwargs)
+    var_x = blocks.upscale(var_x, 256, res_block_follows=True, **kwargs)
+    var_x = blocks.res_block(var_x, 256, **kwargs)
+    var_x = blocks.upscale(var_x, input_shape[0], res_block_follows=True, **kwargs)
+    var_x = blocks.res_block(var_x, input_shape[0], **kwargs)
+    var_x = blocks.conv2d(var_x, 3,
+                                kernel_size=5,
+                                padding="same",
+                                activation="sigmoid",
+                                name="face_out")
+    outputs = [var_x]
+    return Model(input_, outputs=outputs)
+
+def decoder_B():
+    """ Decoder Network """
+    global kernel_initializer
+    global input_shape
+    kwargs = dict(kernel_initializer=kernel_initializer)
+    decoder_shape = input_shape[0] // 8
+    input_ = Input(shape=(decoder_shape, decoder_shape, 512))
+
+    var_x = input_
+    var_x = blocks.upscale(var_x, 512, res_block_follows=True, **kwargs)
+    var_x = blocks.res_block(var_x, 512, **kwargs)
+    var_x = blocks.upscale(var_x, 256, res_block_follows=True, **kwargs)
+    var_x = blocks.res_block(var_x, 256, **kwargs)
+    var_x = blocks.upscale(var_x, input_shape[0], res_block_follows=True, **kwargs)
+    var_x = blocks.res_block(var_x, input_shape[0], **kwargs)
+    var_x = blocks.conv2d(var_x, 3,
+                                kernel_size=5,
+                                padding="same",
+                                activation="sigmoid",
+                                name="face_out")
+    outputs = [var_x]
+    return Model(input_, outputs=outputs)
+
+
+#resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu='grpc://' + os.environ['COLAB_TPU_ADDR'])
+#tf.config.experimental_connect_to_cluster(resolver)
+## This is the TPU initialization code that has to be at the beginning.
+#tf.tpu.experimental.initialize_tpu_system(resolver)
+#strategy = tf.distribute.experimental.TPUStrategy(resolver)
+#try:
+    #with strategy.scope():
+encoder = Encoder()
+decoder_A = Decoder()
+decoder_B = Decoder()
+
+var_x = Input( shape=IMAGE_SHAPE )
+var_y = Input( shape=IMAGE_SHAPE )
+#autoencoder_A = Model( x, decoder_A( encoder(x) ) )
+#autoencoder_B = Model( x, decoder_B( encoder(x) ) )
+dual_model = Model(inputs=[x, y],
+                    outputs=[decoder_A(encoder(var_x)), decoder_B(encoder(var_y))])
+dual_model.compile(optimizer=optimizer, loss='mean_absolute_error')
     print("compiled")
-except:
-    print("error")
+#except:
+#    print("error")
 #autoencoder_A.compile( optimizer=optimizer, loss='mean_absolute_error' )
 #autoencoder_B.compile( optimizer=optimizer, loss='mean_absolute_error' )
 
---
+++ model_tools.py
@@ -0,0 +1,150 @@
+import tensorflow as tf
+from tf.keras.layers import (add, Add, BatchNormalization, concatenate, Lambda, regularizers,
+                          Permute, Reshape, SeparableConv2D, Softmax, UpSampling2D)
+from tf.keras.layers.advanced_activations import LeakyReLU
+from tf.keras.layers.convolutional import Conv2D
+from tf.keras.layers.core import Activation
+from tf.keras.initializers import he_uniform, VarianceScaling
+from pixel_shuffler import PixelShuffler
+
+class NNBlocks():
+    names = dict()
+    first_run = 1
+    use_subpixel = 0
+    use_icnr_init = 0
+    use_convaware_init = 0
+    use_reflect_padding = 0
+
+    def get_name(name):
+        """ Return unique layer name for requested block """
+        names[name] = names.setdefault(name, -1) + 1
+        name = "{}_{}".format(name, names[name])
+        return name
+
+    def set_default_initializer(kwargs):
+    """ Sets the default initializer for conv2D and Seperable conv2D layers
+        to conv_aware or he_uniform().
+        if a specific initializer has been passed in then the specified initializer
+        will be used rather than the default """
+    if "kernel_initializer" in kwargs:
+        return kwargs
+    default = he_uniform()
+    if kwargs.get("kernel_initializer", None) != default:
+        kwargs["kernel_initializer"] = default
+    return kwargs
+
+
+    def conv2d(inp, filters, kernel_size, strides=(1, 1), padding="same", **kwargs):
+        """ A standard conv2D layer with correct initialization """
+        if kwargs.get("name", None) is None:
+            kwargs["name"] = get_name("conv2d_{}".format(inp.shape[1]))
+        kwargs = set_default_initializer(kwargs)
+        var_x = Conv2D(filters, kernel_size,
+                       strides=strides,
+                       padding=padding,
+                       **kwargs)(inp)
+        return var_x
+
+    # <<< Original Model Blocks >>> #
+    def conv(inp, filters, kernel_size=5, strides=2, padding="same",
+             use_instance_norm=False, res_block_follows=False, **kwargs):
+        """ Convolution Layer"""
+        name = get_name("conv_{}".format(inp.shape[1]))
+#        if use_reflect_padding:
+#            inp = ReflectionPadding2D(stride=strides,
+#                                      kernel_size=kernel_size,
+#                                      name="{}_reflectionpadding2d".format(name))(inp) //TODO
+#            padding = "valid"
+        var_x = conv2d(inp, filters,
+                            kernel_size=kernel_size,
+                            strides=strides,
+                            padding=padding,
+                            name="{}_conv2d".format(name),
+                            **kwargs)
+        #if use_instance_norm:
+        #    var_x = InstanceNormalization(name="{}_instancenorm".format(name))(var_x)
+        #if not res_block_follows:
+        #    var_x = LeakyReLU(0.1, name="{}_leakyrelu".format(name))(var_x) //TODO
+        return var_x
+
+    def upscale(inp, filters, kernel_size=3, padding="same",
+                use_instance_norm=False, res_block_follows=False, scale_factor=2, **kwargs):
+        """ Upscale Layer """
+        name = get_name("upscale_{}".format(inp.shape[1]))
+#        if use_reflect_padding:
+#            inp = ReflectionPadding2D(stride=1,
+#                                      kernel_size=kernel_size,
+#                                      name="{}_reflectionpadding2d".format(name))(inp)
+#            padding = "valid"
+        kwargs = set_default_initializer(kwargs)
+#        if use_icnr_init:
+#            original_init = switch_kernel_initializer(
+#                kwargs,
+#                ICNR(initializer=kwargs["kernel_initializer"]))
+        var_x = conv2d(inp, filters * scale_factor * scale_factor,
+                            kernel_size=kernel_size,
+                            padding=padding,
+                            name="{}_conv2d".format(name),
+                            **kwargs)
+#        if use_icnr_init:
+#            switch_kernel_initializer(kwargs, original_init)
+#        if use_instance_norm:
+#            var_x = InstanceNormalization(name="{}_instancenorm".format(name))(var_x)
+#        if not res_block_follows:
+#            var_x = LeakyReLU(0.1, name="{}_leakyrelu".format(name))(var_x)
+#        if use_subpixel:
+#            var_x = SubPixelUpscaling(name="{}_subpixel".format(name),
+#                                      scale_factor=scale_factor)(var_x) //TODO
+#        else:
+        var_x = PixelShuffler(name="{}_pixelshuffler".format(name), size=scale_factor)(var_x)
+        return var_x
+
+    # <<< DFaker Model Blocks >>> #
+    def res_block(inp, filters, kernel_size=3, padding="same", **kwargs):
+        """ Residual block """
+        name = get_name("residual_{}".format(inp.shape[1]))
+        var_x = LeakyReLU(alpha=0.2, name="{}_leakyrelu_0".format(name))(inp)
+#        if use_reflect_padding:
+#            var_x = ReflectionPadding2D(stride=1,
+#                                        kernel_size=kernel_size,
+#                                        name="{}_reflectionpadding2d_0".format(name))(var_x)
+#            padding = "valid"
+        var_x = conv2d(var_x, filters,
+                            kernel_size=kernel_size,
+                            padding=padding,
+                            name="{}_conv2d_0".format(name),
+                            **kwargs)
+        var_x = LeakyReLU(alpha=0.2, name="{}_leakyrelu_1".format(name))(var_x)
+#        if use_reflect_padding:
+#            var_x = ReflectionPadding2D(stride=1,
+#                                        kernel_size=kernel_size,
+#                                        name="{}_reflectionpadding2d_1".format(name))(var_x)
+#            padding = "valid"
+#        if not use_convaware_init:
+#            original_init = switch_kernel_initializer(kwargs, VarianceScaling(
+#                scale=0.2,
+#                mode="fan_in",
+#                distribution="uniform"))
+#        var_x = conv2d(var_x, filters,
+#                            kernel_size=kernel_size,
+#                            padding=padding,
+#                            **kwargs)
+#        if not use_convaware_init:
+#            switch_kernel_initializer(kwargs, original_init)
+        var_x = Add()([var_x, inp])
+        var_x = LeakyReLU(alpha=0.2, name="{}_leakyrelu_3".format(name))(var_x)
+        return var_x
+def conv_sep(inp, filters, kernel_size=5, strides=2, **kwargs):
+    """ Seperable Convolution Layer """
+    name = get_name("separableconv2d_{}".format(inp.shape[1]))
+    kwargs = set_default_initializer(kwargs)
+    var_x = SeparableConv2D(filters,
+                            kernel_size=kernel_size,
+                            strides=strides,
+                            padding="same",
+                            name="{}_seperableconv2d".format(name),
+                            **kwargs)(inp)
+    var_x = Activation("relu", name="{}_relu".format(name))(var_x)
+    return var_x
+
+
