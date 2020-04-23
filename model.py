1739ca771df509bf73775dd7f0bcd3678225d9c9
fff <dimajkovlev865@gmail.com>
2020-04-24 01:00:40 +0300

    fix

--- model.py
+++ model.py
@@ -2,15 +2,15 @@ import tensorflow as tf
 import os
 
 from tensorflow.python.keras.models import Model
-from tensorflow.python.keras.layers import Input, Dense, Flatten, Reshape, 
+from tensorflow.python.keras.layers import Input, Dense, Flatten, Reshape
 from tensorflow.python.keras.layers.advanced_activations import LeakyReLU
 from tensorflow.python.keras.layers.convolutional import Conv2D
 from tensorflow.keras.optimizers import Adam
 from pixel_shuffler import PixelShuffler
-from model_tools import NNBlocks
-from tf.keras.initializers import RandomNormal
-
-blocks = NNBlocks()
+import model_tools
+from tensorflow.keras.initializers import RandomNormal
+from tensorflow.keras.initializers import RandomNormal
+from tensorflow.keras.layers import add, Dense, Flatten, Input, Reshape
 
 #from tensorflow.python.framework.ops import disable_eager_execution
 
@@ -64,7 +64,7 @@ ENCODER_DIM = 1024
 kernel_initializer = RandomNormal(0, 0.02)
 input_shape = (128, 128, 3)
 encoder_dim = 1024
-def encoder():
+def Encoder():
     """ Encoder Network """
     global kernel_initializer
     global input_shape
@@ -73,29 +73,29 @@ def encoder():
     in_conv_filters = input_shape[0]
     dense_shape = input_shape[0] // 16
 
-    var_x = blocks.conv(input_, in_conv_filters, res_block_follows=True, **kwargs)
+    var_x = model_tools.conv(input_, in_conv_filters, res_block_follows=True, **kwargs)
     tmp_x = var_x
     res_cycles = 16
     for _ in range(res_cycles):
-        nn_x = blocks.res_block(var_x, in_conv_filters, **kwargs)
+        nn_x = model_tools.res_block(var_x, in_conv_filters, **kwargs)
         var_x = nn_x
     # consider adding scale before this layer to scale the residual chain
     var_x = add([var_x, tmp_x])
-    var_x = blocks.conv(var_x, 128, **kwargs)
+    var_x = model_tools.conv(var_x, 128, **kwargs)
     var_x = PixelShuffler()(var_x)
-    var_x = blocks.conv(var_x, 128, **kwargs)
+    var_x = model_tools.conv(var_x, 128, **kwargs)
     var_x = PixelShuffler()(var_x)
-    var_x = blocks.conv(var_x, 128, **kwargs)
-    var_x = blocks.conv_sep(var_x, 256, **kwargs)
-    var_x = blocks.conv(var_x, 512, **kwargs)
-    var_x = blocks.conv_sep(var_x, 1024, **kwargs)
+    var_x = model_tools.conv(var_x, 128, **kwargs)
+    var_x = model_tools.conv_sep(var_x, 256, **kwargs)
+    var_x = model_tools.conv(var_x, 512, **kwargs)
+    var_x = model_tools.conv_sep(var_x, 1024, **kwargs)
     var_x = Dense(encoder_dim, **kwargs)(Flatten()(var_x))
     var_x = Dense(dense_shape * dense_shape * 1024, **kwargs)(var_x)
     var_x = Reshape((dense_shape, dense_shape, 1024))(var_x)
-    var_x = blocks.upscale(var_x, 512, **kwargs)
+    var_x = model_tools.upscale(var_x, 512, **kwargs)
     return Model(input_, var_x)
 
-def decoder_A():
+def Decoder():
     """ Decoder Network """
     global kernel_initializer
     global input_shape
@@ -104,13 +104,13 @@ def decoder_A():
     input_ = Input(shape=(decoder_shape, decoder_shape, 512))
 
     var_x = input_
-    var_x = blocks.upscale(var_x, 512, res_block_follows=True, **kwargs)
-    var_x = blocks.res_block(var_x, 512, **kwargs)
-    var_x = blocks.upscale(var_x, 256, res_block_follows=True, **kwargs)
-    var_x = blocks.res_block(var_x, 256, **kwargs)
-    var_x = blocks.upscale(var_x, input_shape[0], res_block_follows=True, **kwargs)
-    var_x = blocks.res_block(var_x, input_shape[0], **kwargs)
-    var_x = blocks.conv2d(var_x, 3,
+    var_x = model_tools.upscale(var_x, 512, res_block_follows=True, **kwargs)
+    var_x = model_tools.res_block(var_x, 512, **kwargs)
+    var_x = model_tools.upscale(var_x, 256, res_block_follows=True, **kwargs)
+    var_x = model_tools.res_block(var_x, 256, **kwargs)
+    var_x = model_tools.upscale(var_x, input_shape[0], res_block_follows=True, **kwargs)
+    var_x = model_tools.res_block(var_x, input_shape[0], **kwargs)
+    var_x = model_tools.conv2d(var_x, 3,
                                 kernel_size=5,
                                 padding="same",
                                 activation="sigmoid",
@@ -118,28 +118,28 @@ def decoder_A():
     outputs = [var_x]
     return Model(input_, outputs=outputs)
 
-def decoder_B():
-    """ Decoder Network """
-    global kernel_initializer
-    global input_shape
-    kwargs = dict(kernel_initializer=kernel_initializer)
-    decoder_shape = input_shape[0] // 8
-    input_ = Input(shape=(decoder_shape, decoder_shape, 512))
-
-    var_x = input_
-    var_x = blocks.upscale(var_x, 512, res_block_follows=True, **kwargs)
-    var_x = blocks.res_block(var_x, 512, **kwargs)
-    var_x = blocks.upscale(var_x, 256, res_block_follows=True, **kwargs)
-    var_x = blocks.res_block(var_x, 256, **kwargs)
-    var_x = blocks.upscale(var_x, input_shape[0], res_block_follows=True, **kwargs)
-    var_x = blocks.res_block(var_x, input_shape[0], **kwargs)
-    var_x = blocks.conv2d(var_x, 3,
-                                kernel_size=5,
-                                padding="same",
-                                activation="sigmoid",
-                                name="face_out")
-    outputs = [var_x]
-    return Model(input_, outputs=outputs)
+#def Decoder_B():
+#    """ Decoder Network """
+#    global kernel_initializer
+#    global input_shape
+#    kwargs = dict(kernel_initializer=kernel_initializer)
+#    decoder_shape = input_shape[0] // 8
+#    input_ = Input(shape=(decoder_shape, decoder_shape, 512))
+#
+#    var_x = input_
+#    var_x = model_tools.upscale(var_x, 512, res_block_follows=True, **kwargs)
+#    var_x = model_tools.res_block(var_x, 512, **kwargs)
+#    var_x = model_tools.upscale(var_x, 256, res_block_follows=True, **kwargs)
+#    var_x = model_tools.res_block(var_x, 256, **kwargs)
+#    var_x = model_tools.upscale(var_x, input_shape[0], res_block_follows=True, **kwargs)
+#    var_x = model_tools.res_block(var_x, input_shape[0], **kwargs)
+#    var_x = model_tools.conv2d(var_x, 3,
+#                                kernel_size=5,
+#                                padding="same",
+#                                activation="sigmoid",
+#                                name="face_out")
+#    outputs = [var_x]
+#    return Model(input_, outputs=outputs)
 
 
 #resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu='grpc://' + os.environ['COLAB_TPU_ADDR'])
@@ -153,14 +153,14 @@ encoder = Encoder()
 decoder_A = Decoder()
 decoder_B = Decoder()
 
-var_x = Input( shape=IMAGE_SHAPE )
-var_y = Input( shape=IMAGE_SHAPE )
+var_x = Input( shape=input_shape )
+var_y = Input( shape=input_shape )
 #autoencoder_A = Model( x, decoder_A( encoder(x) ) )
 #autoencoder_B = Model( x, decoder_B( encoder(x) ) )
-dual_model = Model(inputs=[x, y],
+dual_model = Model(inputs=[var_x, var_y],
                     outputs=[decoder_A(encoder(var_x)), decoder_B(encoder(var_y))])
 dual_model.compile(optimizer=optimizer, loss='mean_absolute_error')
-    print("compiled")
+print("compiled")
 #except:
 #    print("error")
 #autoencoder_A.compile( optimizer=optimizer, loss='mean_absolute_error' )
